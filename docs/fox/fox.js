0088437e5bd26a17b13c85f82850f6f2bc269ed1
0xdya <178745289+0xdya@users.noreply.github.com>
2025-06-21 22:17:00 +0100

    Add files via upload

---
+++ docs/fox/fox.js
@@ -0,0 +1,8 @@
+    lottie.loadAnimation({
+      container: document.getElementById('lottie'),
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: './fox/2.json' // المسار المحلي لملف JSON
+    });
+    
